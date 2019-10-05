08dec39c02533b94bf625481b0aad1b0e188070b
delubiod <delubiod@gmail.com>
2019-10-04 23:43:32 -0300

    added a back button to the external website

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -5,6 +5,15 @@ import styles from "../css/postlist.module.css";
 const PostList = ({ posts }) => {
   return (
     <section className={styles.posts}>
+      <div>
+        <a
+          className={styles.link}
+          href="http://www.centromedicomarista.com"
+          rel="noopener noreferrer"
+        >
+          Voltar Ao Site Do Centro Médico Marista
+        </a>
+      </div>
       <h1>Daniel Marques Novaes</h1>
       <h4>Blog</h4>
       <div className={styles.center}>
